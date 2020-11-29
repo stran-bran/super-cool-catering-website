272f02fbb5f924f5c4e6c75a372109a95d764b13
stran-bran <sonkut1234@gmail.com>
2020-11-29 14:21:26 -0500

    claen up the master pages code. Added empty profile pages

---
+++ CatererService/Pages/ProfilePage/CatererProfile.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CatererService.Pages.ProfilePage
+{
+    public partial class CatererProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
