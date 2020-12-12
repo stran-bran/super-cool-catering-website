7eb5be9c410c2a1db5ee977abba6207aec34b2bf
ThanhNgan <tngantra@confederationcollege.ca>
2020-12-11 23:48:51 -0500

    Modifile home  page, add caterer lists, and catererManaging.

--- CatererService/CatererList.ascx.designer.cs
+++ CatererService/CatererList.ascx.designer.cs
@@ -7,29 +7,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CatererService.Pages.Caterer_Managing_page
+namespace CatererService
 {
 
 
-    public partial class CatererManaging_Admin_
+    public partial class CatererList
     {
 
         /// <summary>
-        /// FormView1 control.
+        /// DataList1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.FormView FormView1;
+        protected global::System.Web.UI.WebControls.DataList DataList1;
 
         /// <summary>
-        /// SqlDataSource1 control.
+        /// EntityDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        protected global::System.Web.UI.WebControls.EntityDataSource EntityDataSource1;
     }
 }
--- CatererService/Master/Master1.Master.designer.cs
+++ CatererService/Master/Master1.Master.designer.cs
@@ -87,24 +87,6 @@ namespace CatererService.Master
         protected global::System.Web.UI.WebControls.LoginStatus LoginStatus1;
 
         /// <summary>
-        /// Menu1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu Menu1;
-
-        /// <summary>
-        /// SiteMapDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
-
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
