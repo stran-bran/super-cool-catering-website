13a9f4b3d77d47ebda3e6be276d5b64ecf999d41
stran-minh <sonkut1234@gmail.com>
2020-12-17 18:40:03 -0500

    changed master page design

--- CatererService/Master/Master1.Master.designer.cs
+++ CatererService/Master/Master1.Master.designer.cs
@@ -60,15 +60,6 @@ namespace CatererService.Master
         protected global::System.Web.UI.WebControls.Button ButtonSearch;
 
         /// <summary>
-        /// HyperLink1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
-
-        /// <summary>
         /// cart control.
         /// </summary>
         /// <remarks>
@@ -76,6 +67,15 @@ namespace CatererService.Master
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink cart;
+
+        /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
 
         /// <summary>
         /// LoginStatus1 control.
