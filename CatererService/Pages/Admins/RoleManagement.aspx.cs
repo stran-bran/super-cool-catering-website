e72bc44a745485fbed4b6d0c2e30e2307e4ccede
stran-minh <sonkut1234@gmail.com>
2020-12-18 04:23:59 -0500

    implementedted the rolechanging code however i think my browser crash when executing it

---
+++ CatererService/Pages/Admins/RoleManagement.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CatererService.Pages.Admins
+{
+    public partial class RoleManagement : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack) {
+                UsersRoleList.DataBind();
+                ddlUserList.DataBind();
+                ddlUserList.SelectedIndex = 0;
+            }
+        }
+
+        protected void btnNewRoles_Click(object sender, EventArgs e)
+        {
+            String roleName = txtRoleName.Text.Trim();
+            
+            if (!System.Web.Security.Roles.RoleExists(roleName)) 
+            {
+                try
+                {
+                    System.Web.Security.Roles.CreateRole(roleName);
+                    lblActionStatus.Text = $"Role (<strong>){roleName} was created successfully";
+                    grdRoleList.DataBind();
+                }
+                catch (Exception ex) {
+                    lblActionStatus.Text = $"Role {roleName} was not created successfully.";
+                }
+            }
+            
+        }
+
+        protected void ddUserList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CheckRolesForSelectedUser();
+        }
+
+        private void CheckRolesForSelectedUser()
+        {
+            string selectUserName = ddlUserList.SelectedValue;
+            string[] selectedUserRoles = System.Web.Security.Roles.GetRolesForUser(selectUserName);
+
+            foreach (RepeaterItem ri in UsersRoleList.Items) {
+                CheckBox RoleCheckBox = ri.FindControl("RoleCheckBox") as CheckBox;
+
+                if (selectedUserRoles.Contains<string>(RoleCheckBox.Text)) 
+                RoleCheckBox.Checked = true;
+                else
+                    RoleCheckBox.Checked = false;
+            }
+            throw new NotImplementedException();
+        }
+
+        protected void RoleCheckBox_CheckChanged(object sender, EventArgs e) {
+            //Reference check box raise the event 
+            CheckBox RoleCheckBox = sender as CheckBox;
+
+            string selectedUserName = ddlUserList.SelectedValue;
+            string roleName = RoleCheckBox.Text;
+
+            if (RoleCheckBox.Checked)
+            {
+                System.Web.Security.Roles.AddUserToRole(selectedUserName, roleName);
+                lblActionStatus.Text = string.Format("User {0} was added to {1}", selectedUserName, roleName);
+            }
+            else {
+                System.Web.Security.Roles.RemoveUserFromRole(selectedUserName, roleName);
+                lblActionStatus.Text = string.Format("User {0} was removed from role {1}", selectedUserName, roleName);
+            }
+        }
+    }
+}
--- CatererService/Pages/Register page/Register.aspx
+++ CatererService/Pages/Register page/Register.aspx
@@ -2,13 +2,20 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <asp:CreateUserWizard ID="CreateUserWizard1" runat="server" Height="329px" Width="393px" ContinueDestinationPageUrl="~/Pages/Home page/Home.aspx">
+    <%--<asp:CreateUserWizard ID="CreateUserWizard1" runat="server" Height="329px" Width="393px" ContinueDestinationPageUrl="~/Pages/Home page/Home.aspx">
     <MailDefinition BodyFileName="~/Confirmation email/SignUpConfirmation.txt" Subject="Your New Account at CP380 Website">
         </MailDefinition>
         <WizardSteps>
         <asp:CreateUserWizardStep runat="server" />
         <asp:CompleteWizardStep runat="server" />
     </WizardSteps>
-</asp:CreateUserWizard>
-
+</asp:CreateUserWizard>--%>
+    <asp:CreateUserWizard ID="CreateUserWizard1" runat="server" ContinueDestinationPageUrl="~/Pages/Home page/Home.aspx">
+        <WizardSteps>
+            <asp:CreateUserWizardStep ID="CreateUserWizardStep1" runat="server">
+            </asp:CreateUserWizardStep>
+            <asp:CompleteWizardStep ID="CompleteWizardStep1" runat="server">
+            </asp:CompleteWizardStep>
+        </WizardSteps>
+    </asp:CreateUserWizard>
 </asp:Content>
